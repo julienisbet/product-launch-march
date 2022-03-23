33a05671cdd6748b54046d80c4100f029e330a38
Julie Nisbet <jmnisbet@gmail.com>
2022-03-22 19:05:18 -0700

    adding js toggling

--- app.js
+++ app.js
@@ -17,9 +17,12 @@ const button = document.getElementById('button');
 button.addEventListener('click', ()=>{
     // 1. store the element with id email-inputs
     //  as a variable inputs
+    const inputs = document.getElementById('email-inputs');
     // 2. add the class "hide" to inputs
+    inputs.classList.toggle('hide');
     // 3. store the element with the id thank-you
     //   as a variable thankYou
+    const thankYou = document.getElementById('thank-you');
     // 4. remove the class "hide" from thankYou
-    
+    thankYou.classList.toggle('hide');
 });
