f3a249fd254615bab44d0ef9a4b6cf4c55522ae2
Julie Nisbet <jmnisbet@gmail.com>
2022-03-22 19:00:38 -0700

    adding plan for JS

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
-console.log('hello from app.js');
+// console.log('hello from app.js');
 
-let dog = 'Ben Donich';
+// let dog = 'Ben Donich';
 
-console.log(`My dog's name is ${dog}`);
-console.log(`${dog} is very cute.`);
+// console.log(`My dog's name is ${dog}`);
+// console.log(`${dog} is very cute.`);
 
 // const label = document.getElementById('label');
 // // label.textContent = 'awesome blossom';
 // label.style.color = 'blue';
 // label.style.fontSize = '4rem';
 
-const darkText = document.querySelector('.dark-text');
-console.log(darkText)
+// const darkText = document.querySelector('.dark-text');
+// console.log(darkText)
+
+const button = document.getElementById('button');
+button.addEventListener('click', ()=>{
+    // 1. store the element with id email-inputs
+    //  as a variable inputs
+    // 2. add the class "hide" to inputs
+    // 3. store the element with the id thank-you
+    //   as a variable thankYou
+    // 4. remove the class "hide" from thankYou
+    
+});
